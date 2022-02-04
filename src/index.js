2eefffaecb8f6d907c0b00ff7f6ea944f1c1d870
Andrew Smith <a.smithb21@gmail.com>
2022-02-04 16:47:35 -0600

    set up index.js to run server

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const MarvelAPI = require('./datasources/marvel-api');
+
+async function startApolloServer(typeDefs, resolvers) {
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        dataSources: () => {
+            return {
+                marvelAPI: new MarvelAPI(),
+            };
+        },
+    });
+
+    const {url, port} = await server.listen({port: 4000});
+    console.log(`
+        Server is running
+        Listening on port ${port}
+        Query at ${url}
+    `);
+};
+
+startApolloServer(typeDefs, resolvers);
