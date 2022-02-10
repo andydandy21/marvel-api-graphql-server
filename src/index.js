ee622d9d27714beaed2e3cc4c46ea6b96774d911
Andrew Smith <a.smithb21@gmail.com>
2022-02-10 15:29:26 -0600

    adjusted project for serverless deployment

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
 require('dotenv').config();
 
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-lambda');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const MarvelAPI = require('./datasources/marvel-api');
 
-async function startApolloServer(typeDefs, resolvers) {
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-        dataSources: () => {
-            return {
-                marvelAPI: new MarvelAPI(),
-            };
-        },
-    });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+        return {
+            marvelAPI: new MarvelAPI(),
+        };
+    },
+});
 
-    const {url, port} = await server.listen({port: 4000});
-    console.log(`
-        Server is running
-        Listening on port ${port}
-        Query at ${url}
-    `);
-};
-
-startApolloServer(typeDefs, resolvers);
+exports.graphqlHandler = server.createHandler();
